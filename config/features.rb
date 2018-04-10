81ecaa91207cda72bfceb9832a6ec0ddbe93dd1d
Rob Kaufman <rob@notch8.com>
2018-04-10 15:34:40 -0700

    updated to sufia 7.4.1

--- config/features.rb
+++ config/features.rb
@@ -1,8 +1,8 @@
 Flipflop.configure do
   # Strategies will be used in the order listed here.
-  strategy :cookie
-  strategy :active_record
-  strategy :default
+  #strategy :cookie
+  #strategy :active_record
+  #strategy :default
 
   # Other strategies:
   #
