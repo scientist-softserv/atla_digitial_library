2bfad2f4f4e0cca490de37fcfbf636e38044d424
Rob Kaufman <rob@notch8.com>
2018-04-11 12:16:57 -0700

    stop at 100 for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,6 @@ end
 Rake::Task['sufia:default_admin_set:create'].invoke
 Rake::Task['curation_concerns:workflow:load'].invoke
 Rake::Task['sufia:migrate:move_all_works_to_admin_set']
-Rake::Task['import:import_ptc_oia'].invoke('rob@notch8.com',true)
+Rake::Task['import:import_ptc_oia'].invoke('rob@notch8.com', 'true')
 
 #ActiveFedora::Base.reindex_everything
