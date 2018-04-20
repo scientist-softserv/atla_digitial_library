d74c10f81500fb9d4557b5e76500453c9221e6cd
Rob Kaufman <rob@notch8.com>
2018-04-19 17:38:06 -0700

    removes state listing below title and admin set link on work show

--- config/initializers/healthcheck.rb
+++ config/initializers/healthcheck.rb
@@ -1,6 +1,8 @@
 HealthMonitor.configure do |config|
   config.cache
-  config.redis
+  config.redis.configure do |redis_config|
+    redis_config.connection = Redis.current # use your custom redis connection
+  end
   config.sidekiq.configure do |sidekiq_config|
     sidekiq_config.latency = 3.hours
     sidekiq_config.queue_size = 50
