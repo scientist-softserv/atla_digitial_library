436addb80392b2773e888136e47a8ee7e3541b8d
George Wheeler <george@dewey4.com>
2018-08-10 11:34:58 -0700

    change target to format_digital

--- app/harvesters/oai/ptc/record_parser.rb
+++ app/harvesters/oai/ptc/record_parser.rb
@@ -4,7 +4,7 @@ module OAI::PTC
     matcher 'creator', split: true
     matcher 'date', from: ['date'], split: true
     matcher 'description'
-    matcher 'format_original', from: ['format_digital', 'format'], parsed: true
+    matcher 'format_digital', from: ['format_digital', 'format'], parsed: true
     matcher 'publisher'
     matcher 'language', parsed: true, split: true
     matcher 'identifier', from: ['identifier'], if: ->(parser, content) { content.match(/http(s{0,1}):\/\//) }
