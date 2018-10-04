d1eaa15f7e7b28715b26a7b27be92bb5de439f01
Tom Johnson <tom@notch8.com>
2018-10-04 16:27:41 -0700

    Hard code `Collection#bytes` to 0
    The `#bytes` implementation is still prohibitively slow. https://github.com/samvera/hyrax/blob/5a9d1be16ee1a9150646384471992b03aab527a5/app/models/concerns/hyrax/collection_behavior.rb#L96-L108

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Collection do
-  it "has tests" do
-    skip "Add your tests here"
+  subject(:collection) { described_class.new }
+
+  describe '#bytes' do
+    it 'is 0' do
+      expect(collection.bytes).to eq 0
+    end
+
+    context 'with members' do
+      it 'is hard coded to 0'
+    end
   end
 end
