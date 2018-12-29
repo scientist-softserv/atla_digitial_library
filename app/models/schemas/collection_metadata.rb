aa88e7a99b4e360aede0cca7e004a183d27c2676
Rob Kaufman <rob@notch8.com>
2018-12-29 13:25:28 -0800

    update collection schema, make sure work metadata and collection metadata stay out of each others way

--- app/models/schemas/collection_metadata.rb
+++ app/models/schemas/collection_metadata.rb
@@ -2,6 +2,8 @@
 module Schemas
   class CollectionMetadata < ActiveTriples::Schema
     property :file_name,   predicate: RDF::Vocab::DC.MediaTypeOrExtent # required
+    property :date, predicate: ::RDF::Vocab::DC11.date
+    property :contributing_institution, predicate: ::RDF::Vocab::DC.contributor
     property :institution, predicate: RDF::Vocab::FOAF.based_near
     property :name_code,   predicate: RDF::Vocab::DC.identifier # required
     property :pub_place,   predicate: RDF::Vocab::DC.Location
