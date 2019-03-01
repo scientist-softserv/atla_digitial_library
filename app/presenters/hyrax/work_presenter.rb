f0ac0626016f1901943f344cc0fcfea192bcdccf
Rob Kaufman <rob@notch8.com>
2019-02-28 17:47:01 -0800

    add new field for manifests, move it through to show universal viewer

--- app/presenters/hyrax/work_presenter.rb
+++ app/presenters/hyrax/work_presenter.rb
@@ -5,7 +5,8 @@ module Hyrax
     delegate :title, :date_created, :description,
              :creator, :contributor, :subject, :publisher, :language, :embargo_release_date,
              :lease_expiration_date, :license, :source, :rights_statement, :thumbnail_id, :representative_id,
-             :rendering_ids, :member_of_collection_ids, :contributing_institution, :place, :extent, :format_original, :format_digital, :time_period, :alternative_title, :types,
+             :rendering_ids, :member_of_collection_ids, :contributing_institution, :place, :extent, :format_original,
+             :format_digital, :time_period, :alternative_title, :types, :manifest_url,
 
              to: :solr_document
 
