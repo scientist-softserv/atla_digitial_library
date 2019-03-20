59e74572c439abb8dbb40edbe6f910bc7e785548
Rob Kaufman <rob@notch8.com>
2019-03-20 15:42:55 -0700

    quick fix for prod mode

--- app/indexers/atla_collection_metadata_indexer.rb
+++ app/indexers/atla_collection_metadata_indexer.rb
@@ -1,5 +1,5 @@
 ## adding additional fields here
 # This class gets called by ActiveFedora::IndexingService#olrize_rdf_assertions
-class AtlaCollectionMetadataIndexer < BasicMetadataIndexer
+class AtlaCollectionMetadataIndexer < Hyrax::BasicMetadataIndexer
   self.stored_and_facetable_fields = %i[resource_type creator contributor keyword publisher subject language based_near contributing_institution date slug]
 end
