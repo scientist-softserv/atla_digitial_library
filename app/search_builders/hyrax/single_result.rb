155f0af5abd52545daef38eb2b00c4da225c2e1d
Kiah <kiah@notch8.com>
2019-06-05 10:51:51 -0700

    add documentation to search_builder override file

--- app/search_builders/hyrax/single_result.rb
+++ app/search_builders/hyrax/single_result.rb
@@ -1,3 +1,5 @@
+# Override file from Hyrax 2.3.3
+# Overrides are marked with an "OVERRIDE" comment
 module Hyrax
   module SingleResult
     extend ActiveSupport::Concern
@@ -6,6 +8,8 @@ module Hyrax
       self.default_processor_chain += [:find_one]
     end
 
+    # OVERRIDE METHOD
+    # Tell Solr to search by :slug if it can't find the document by :id
     def find_one(solr_parameters)
       solr_parameters[:fq] << "({!raw f=id}#{blacklight_params.fetch(:id)} OR {!raw f=slug_sim}#{blacklight_params.fetch(:id)})"
     end
