73d9c1348bf1d9ff4ba55cb0aaa1482adbcd8244
Kiah <kiah@notch8.com>
2019-07-11 17:49:26 -0700

    WIP show ALL Work's Collections on the Work show page

--- app/controllers/hyrax/works_controller.rb
+++ app/controllers/hyrax/works_controller.rb
@@ -10,5 +10,23 @@ module Hyrax
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::WorkPresenter
+
+    ## Overrides Hyrax method to customize how breadcrumbs appear on Work show pages.
+    #  Original method found at:
+    #  https://github.com/samvera/hyrax/blob/v2.5.1/app/controllers/concerns/hyrax/breadcrumbs_for_works.rb
+    def build_breadcrumbs
+      return add_breadcrumb_to_show_page if action_name == 'show'
+      super
+    end
+
+    ## Custom method for Work show pages. Breadcrumbs reflect a
+    #  hierarchical structure of which Collections the Work belongs to.
+    def add_breadcrumb_to_show_page
+      add_breadcrumb I18n.t('hyrax.controls.home'), hyrax.root_path
+      presenter.ancestor_collections.each do |c|
+        add_breadcrumb "#{c.title.first}", hyrax.collection_path(c)
+      end
+      add_breadcrumb_for_action
+    end
   end
 end
