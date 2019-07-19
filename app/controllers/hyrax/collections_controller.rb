dd0a9b3f429d750d09fa1ae836956fb8bb13dcc3
Kiah <kiah@notch8.com>
2019-07-19 13:27:26 -0700

    customize breadcrumbs on Collection show pages to match the hierarchical structure

--- app/controllers/hyrax/collections_controller.rb
+++ app/controllers/hyrax/collections_controller.rb
@@ -27,6 +27,24 @@ module Hyrax
     end
     # UPGRADE NOTE: end of block
 
+    ## Overrides Hyrax method to customize how breadcrumbs appear on Collection show pages.
+    #  Original method found at:
+    #  https://github.com/samvera/hyrax/blob/v2.5.1/app/controllers/concerns/hyrax/breadcrumbs.rb
+    def build_breadcrumbs
+      add_breadcrumb I18n.t('hyrax.controls.home'), hyrax.root_path
+      add_breadcrumb I18n.t('hyrax.controls.collections'), main_app.collections_path
+      add_breadcrumb_for_parent
+      add_breadcrumb_for_action
+    end
+
+    ## Custom method for Collection show pages. If the current Collection
+    #  has a parent Collection, add a breadcrumb for it.
+    def add_breadcrumb_for_parent
+      return if @curation_concern.member_of_collection_ids.blank?
+      parent_collection = Collection.find(@curation_concern.member_of_collection_ids.first)
+      add_breadcrumb parent_collection.to_s, hyrax.collection_path(parent_collection)
+    end
+
     private
 
     def form
