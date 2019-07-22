289fc45208fee84d475e03b3dde6e285515576e9
Kiah <kiah@notch8.com>
2019-07-22 11:35:52 -0700

    remove migrations that originally came from Bulkrax

--- db/migrate/20190719073619_change_collection_ids_on_entries.bulkrax.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from bulkrax (originally 20190715162044)
-class ChangeCollectionIdsOnEntries < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :bulkrax_entries, :collection_id, :collection_ids
-  end
-end
