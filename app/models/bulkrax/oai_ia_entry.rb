2e7f6e474255936cc0667a2c00dc1817404573ac
Julie Allinson <julie@notch8.com>
2019-08-06 11:27:00 +0100

    Add matcher class definition to oai_ia_entry.rb

--- app/models/bulkrax/oai_ia_entry.rb
+++ app/models/bulkrax/oai_ia_entry.rb
@@ -1,12 +1,16 @@
 module Bulkrax
   class OaiIaEntry < OaiDcEntry
 
+    def self.matcher_class
+      Bulkrax::AtlaOaiMatcher
+    end
+
     # use same matchers as OaiDcEntry (inherit from OaiDcEntry), override date to use parser
     matcher 'date', from: ['date'], parsed: true
 
     def build_metadata
       self.parsed_metadata = {}
-      self.parsed_metadata[Bulkrax.system_identifier_field] ||= [record.header.identifier]
+      self.parsed_metadata[Bulkrax.system_identifier_field] = [record.header.identifier]
 
       record.metadata.children.each do |child|
         child.children.each do |node|
