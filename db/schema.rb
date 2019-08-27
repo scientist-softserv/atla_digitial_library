416f3b2f348588d14fada999c66dc92d46f1f960
Rob Kaufman <rob@notch8.com>
2019-08-26 22:19:22 -0700

    system nievely has issues w/ collection slugs with "edit" in them

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190719073619) do
+ActiveRecord::Schema.define(version: 20190715162044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
