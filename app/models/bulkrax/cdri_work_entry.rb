3d635a016a01d3fb6cabec1088e6e11dc326addf
Rob Kaufman <rob@notch8.com>
2019-10-03 08:27:39 -0700

    update cdri to modern work entry

--- app/models/bulkrax/cdri_work_entry.rb
+++ app/models/bulkrax/cdri_work_entry.rb
@@ -10,21 +10,16 @@ module Bulkrax
       self.new(raw_metadata: raw_metadata).identifier
     end
 
-    # override to provide file directory
-    def build
-      # attributes, files_dir = nil, files = [], user = nil
-      begin
-        Bulkrax::ApplicationFactory.for(factory_class.to_s).new(build_metadata, File.join(parser.parser_fields['upload_path'], collection.name_code.first) , [], user).run
-      rescue => e
-        self.last_error = "#{e.message}\n\n#{e.backtrace}"
-        self.last_error_at = Time.now
-        self.last_exception = e
-      else
-        self.last_error = nil
-        self.last_error_at = nil
-        self.last_exception = nil
-        self.last_succeeded_at = Time.now
-      end
+    def collection
+      @collection ||= Collection.find(self.collection_ids.first) if self.collection_ids.present?
+    end
+
+    def files_path
+      File.join(parser.parser_fields['upload_path'], collection.name_code.first)
+    end
+
+    def factory
+      @factory ||= Bulkrax::ApplicationFactory.for(factory_class.to_s).new(self.parsed_metadata, identifier, files_path, [], user)
     end
 
     def factory_class
@@ -50,12 +45,12 @@ module Bulkrax
         object.respond_to?(key.to_sym)
       end
 
-      self.parsed_metadata['visibility'] = 'open'
-      self.parsed_metadata['rights_statement'] = [parser.parser_fields['rights_statement']]
-      self.parsed_metadata['collections'] ||= []
-      self.parsed_metadata['collections'] << {id: self.collection&.id}
+      add_visibility
+      add_rights_statement
+      add_collections
+
       self.parsed_metadata['file'] = [raw_metadata_xml["ComponentFileName"].downcase] if raw_metadata_xml["ComponentFileName"].present?
-      self.parsed_metadata['identifier'] ||= [self.identifier]
+      self.parsed_metadata[Bulkrax.system_identifier_field] ||= [self.identifier]
       self.parsed_metadata['has_manifest'] = "1"
       return self.parsed_metadata
     end
