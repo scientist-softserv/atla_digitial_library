28a82e95e2e3025308008e9d9b5019b3fdf8706d
Julie Allinson <julie@notch8.com>
2019-10-08 16:30:19 +0100

    retain capitalization in subject strings

--- app/matchers/bulkrax/atla_oai_matcher.rb
+++ app/matchers/bulkrax/atla_oai_matcher.rb
@@ -16,5 +16,14 @@ module Bulkrax
         src
       end
     end
+
+    # override - retain capitalization in existing string, strip spaces and period
+    #  then capitalize first letter
+    def parse_subject(src)
+      string = src.to_s.strip.chomp('.')
+      if string.present?
+        string.slice(0,1).capitalize + string.slice(1..-1)
+      end
+    end
   end
 end
