8c3533fc80ecc5c2120837e5d3e40e8d8e1a4176
Julie Allinson <julie@notch8.com>
2019-12-19 12:07:42 +0000

    remove old unused migrations

--- db/migrate/20190213223226_update_bulkrax_parsers.rb
+++
@@ -1,8 +0,0 @@
-class UpdateBulkraxParsers < ActiveRecord::Migration[5.1]
-  def change
-    Bulkrax::Importer.find_each do |i|
-      i.parser_klass = i.parser_klass.gsub("::Parsers", '')
-      i.save
-    end
-  end
-end
