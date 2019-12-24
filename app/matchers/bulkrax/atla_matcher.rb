0fb458a8a3b5dbec3799f61315309bf5ea136dd8
Julie Allinson <julie@notch8.com>
2019-12-24 12:28:54 +0000

    update bulkrax to 635e321; broaden custom matcher to all entries

---
+++ app/matchers/bulkrax/atla_matcher.rb
@@ -0,0 +1,16 @@
+module Bulkrax
+  class AtlaMatcher < ApplicationMatcher
+    def parse_types(src)
+      Hyrax::ResourceTypesService.label_from_alt(src.to_s.strip)
+    end
+
+    # override - retain capitalization in existing string, strip spaces and period
+    #  then capitalize first letter
+    def parse_subject(src)
+      string = src.to_s.strip.chomp('.')
+      if string.present?
+        string.slice(0,1).capitalize + string.slice(1..-1)
+      end
+    end
+  end
+end
