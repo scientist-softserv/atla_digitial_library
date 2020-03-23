499aba66f56c27d3bc2acedfd858c94df289327b
Rob Kaufman <rob@notch8.com>
2020-03-23 13:27:45 -0700

    add search tips pages

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
   end
 
   get 'participate' => 'hyrax/pages#show', key: 'participate'
+  get 'search_tips' => 'hyrax/pages#show', key: 'search_tips'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
