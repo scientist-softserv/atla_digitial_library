9356328f6329a5566846b72c8f4b3bd58d2bf0c2
George Wheeler <george@dewey4.com>
2020-06-08 10:39:22 -0700

    rename job - both statistical calls will eventually live here

---
+++ app/jobs/update_statistical_data_job.rb
@@ -0,0 +1,18 @@
+require 'rake'
+
+Rake::Task.clear
+AtlaDigitalLibrary::Application.load_tasks
+
+class UpdateStatisticalDataJob < Hyrax::ApplicationJob
+  queue_as :update_statistical_data
+
+  def perform
+    begin
+      Rake::Task['atla:atla:update_announcement_text'].invoke
+
+      self.class.set(wait_until: 1.week.from_now.beginning_of_day).perform_later
+    rescue
+      return false
+    end
+  end
+end
