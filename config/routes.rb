4cd4be57c87a5f33cc4b8197241b2971846a3aaf
George Wheeler <george@dewey4.com>
2020-06-08 10:41:24 -0700

    rm ws

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
     concerns :searchable
     concerns :range_searchable
-
   end
 
   devise_for :users
