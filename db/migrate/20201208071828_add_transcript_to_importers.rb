917656f50f03ad9ff1ec150c9ca942438176a691
Rob Kaufman <rob@notch8.com>
2020-12-08 22:39:39 -0800

    handle nil case on migration

--- db/migrate/20201208071828_add_transcript_to_importers.rb
+++ db/migrate/20201208071828_add_transcript_to_importers.rb
@@ -1,6 +1,7 @@
 class AddTranscriptToImporters < ActiveRecord::Migration[5.1]
   def change
     Bulkrax::Importer.where(parser_klass: "Bulkrax::OaiQualifiedDcParser").find_each do |i|
+      i.field_mapping ||= {}
       i.field_mapping['transcript_url'] = {"from"=>["transcript"]}
       i.save
     end
