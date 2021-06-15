4d400e15dc2035ccbc244af556a3793af04d8ee0
Rob Kaufman <rob@notch8.com>
2021-06-14 22:42:59 -0700

    merge divergent import job code

--- app/jobs/import_url_job.rb
+++ app/jobs/import_url_job.rb
@@ -1,4 +1,4 @@
-# OVERRIDE: Hyrax 2.9 to retry failed downloads
+# Override: Hyrax 2.7 file path selection as path based urls do not work. Retry failed downloads
 require 'uri'
 require 'tmpdir'
 require 'browse_everything/retriever'
@@ -23,8 +23,11 @@ class ImportUrlJob < Hyrax::ApplicationJob
   def perform(file_set, operation, headers = {})
     operation.performing!
     user = User.find_by_user_key(file_set.depositor)
-    uri = URI(file_set.import_url)
-    name = file_set.label
+    begin
+      uri = URI(file_set.import_url)
+    rescue
+      return false
+    end
 
     @file_set = file_set
     @operation = operation
@@ -36,7 +39,7 @@ class ImportUrlJob < Hyrax::ApplicationJob
 
     # @todo Use Hydra::Works::AddExternalFileToFileSet instead of manually
     #       copying the file here. This will be gnarly.
-    copy_remote_file(uri, name, headers) do |f|
+    copy_remote_file(uri, headers) do |f|
       # reload the FileSet once the data is copied since this is a long running task
       file_set.reload
 
@@ -49,63 +52,74 @@ class ImportUrlJob < Hyrax::ApplicationJob
 
   private
 
-    # Download file from uri, yields a block with a file in a temporary directory.
-    # It is important that the file on disk has the same file name as the URL,
-    # because when the file in added into Fedora the file name will get persisted in the
-    # metadata.
-    # @param uri [URI] the uri of the file to download
-    # @param name [String] the human-readable name of the file
-    # @param headers [Hash] the HTTP headers for the GET request (these may contain an authentication token)
-    # @yield [IO] the stream to write to
-    def copy_remote_file(uri, name, headers = {})
-      filename = File.basename(name)
-      dir = Dir.mktmpdir
-      Rails.logger.debug("ImportUrlJob: Copying <#{uri}> to #{dir}")
+  # Download file from uri, yields a block with a file in a temporary directory.
+  # It is important that the file on disk has the same file name as the URL,
+  # because when the file in added into Fedora the file name will get persisted in the
+  # metadata.
+  # @param uri [URI] the uri of the file to download
+  # @yield [IO] the stream to write to
+  def copy_remote_file(uri, headers = {})
+    filename = safe_filename(uri)
+    dir = Dir.mktmpdir
+    Rails.logger.debug("ImportUrlJob: Copying <#{uri}> to #{dir}")
 
-      File.open(File.join(dir, filename), 'wb') do |f|
-        begin
-          write_file(uri, f, headers)
-          yield f
-        rescue StandardError => e
-          send_error(e.message)
-        end
-      end
-      Rails.logger.debug("ImportUrlJob: Closing #{File.join(dir, filename)}")
-    end
-
-    # Send message to user on download failure
-    # @param filename [String] the filename of the file to download
-    # @param error_message [String] the download error message
-    def send_error(error_message)
-      user = User.find_by_user_key(file_set.depositor)
-      @file_set.errors.add('Error:', error_message)
-      Hyrax.config.callback.run(:after_import_url_failure, @file_set, user)
-      @operation.fail!(@file_set.errors.full_messages.join(' '))
-      # Added to retry url downloads
-      raise ImportUrlException, @file_set.errors.full_messages.join(' ')
-    end
-
-    # Write file to the stream
-    # @param uri [URI] the uri of the file to download
-    # @param f [IO] the stream to write to
-    def write_file(uri, f, headers)
-      retriever = BrowseEverything::Retriever.new
-      uri_spec = ActiveSupport::HashWithIndifferentAccess.new(url: uri, headers: headers)
-      retriever.retrieve(uri_spec) do |chunk|
-        f.write(chunk)
-      end
-      f.rewind
-    end
-
-    # Set the import operation status
-    # @param uri [URI] the uri of the file to download
-    # @param f [IO] the stream to write to
-    # @param user [User]
-    def log_import_status(uri, f, user)
-      if Hyrax::Actors::FileSetActor.new(@file_set, user).create_content(f, from_url: true)
-        operation.success!
-      else
-        send_error(uri.path, nil)
+    File.open(File.join(dir, filename), 'wb') do |f|
+      begin
+        write_file(uri, f, headers)
+        yield f
+      rescue StandardError => e
+        send_error(e.message)
       end
     end
+    Rails.logger.debug("ImportUrlJob: Closing #{File.join(dir, filename)}")
+  end
+
+  # Send message to user on download failure
+  # @param filename [String] the filename of the file to download
+  # @param error_message [String] the download error message
+  def send_error(error_message)
+    user = User.find_by_user_key(file_set.depositor)
+    @file_set.errors.add('Error:', error_message)
+    Hyrax.config.callback.run(:after_import_url_failure, @file_set, user)
+    @operation.fail!(@file_set.errors.full_messages.join(' '))
+    # Added to retry url downloads
+    raise ImportUrlException, @file_set.errors.full_messages.join(' ')
+  end
+
+  # Write file to the stream
+  # @param uri [URI] the uri of the file to download
+  # @param f [IO] the stream to write to
+  def write_file(uri, f, headers)
+    retriever = BrowseEverything::Retriever.new
+    uri_spec = { 'url' => uri }.merge(headers)
+    retriever.retrieve(uri_spec) do |chunk|
+      f.write(chunk)
+    end
+    f.rewind
+  end
+
+  # Set the import operation status
+  # @param uri [URI] the uri of the file to download
+  # @param f [IO] the stream to write to
+  # @param user [User]
+  def log_import_status(uri, f, user)
+    if Hyrax::Actors::FileSetActor.new(@file_set, user).create_content(f, from_url: true)
+      operation.success!
+    else
+      send_error(uri.path, nil)
+    end
+  end
+
+  # Make sure the file we write has a usable name
+  # @param uri [URI] the uri of the file to download
+  def safe_filename(uri)
+    begin
+      r = Faraday.head(uri.to_s)
+      return CGI::parse(r.headers['content-disposition'])["filename"][0].gsub("\"", '')
+    rescue
+      filename = File.basename(uri.path)
+      filename.gsub!('/', '')
+      filename.present? ? filename : file_set.id
+    end
+  end
 end
