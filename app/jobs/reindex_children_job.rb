d83f618d8b1332cf2b5d935281ff1e8a9be85493
Rob Kaufman <rob@notch8.com>
2021-07-26 23:31:23 -0700

    add reindx job for collection nesting

---
+++ app/jobs/reindex_children_job.rb
@@ -0,0 +1,9 @@
+class ReindexChildrenJob < Hyrax::ApplicationJob
+
+  def perform(collection_id)
+    ActiveFedora::Base.where("ancestor_collection_ids_tesim: '#{collection_id}' AND has_model_ssim: Work").each do |work|
+      work.reindex_extent = Hyrax::Adapters::NestingIndexAdapter::LIMITED_REINDEX
+      work.update_index
+    end
+  end
+end
