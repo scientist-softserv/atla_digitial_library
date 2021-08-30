c28840da33243b4fb4805ee9a2668e7455c9e289
April Rieger <april@notch8.com>
2021-08-30 22:50:18 +0000

    i330-split-pdf-into-hi-res-jpegs-in-actor-stack

--- spec/actors/hyrax/actors/work_actor_spec.rb
+++ spec/actors/hyrax/actors/work_actor_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::Actors::WorkActor do
-  it "has tests" do
-    skip "Add your tests here"
+  it "exists" do
+    expect(Hyrax::Actors::WorkActor).to be_a(Class)
   end
 end
