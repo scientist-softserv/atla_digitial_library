c02f9eb5fa7fe675dda28e0e4014b78f7d4c0546
Rob Kaufman <rob@notch8.com>
2021-10-31 11:39:42 -0700

    redis fix

--- config/initializers/health_monitor.rb
+++ config/initializers/health_monitor.rb
@@ -2,6 +2,6 @@ HealthMonitor.configure do |config|
   config.cache
   config.redis.configure do |redis_config|
     # redis_config.connection = Redis.current TODO: figure out why this doesn't work but the below does
-	redis_config.url = "redis://redis:6379"
+    redis_config.url = ENV.fetch('REDIS_URL', 'redis://redis:6379')
   end
 end
