3b9f6f0f014b1a40c880db690f0dc276ac041cf6
Sara G <geezy@Notch8-MacBook-Pro.local>
2021-11-24 14:26:21 -0600

    Improve video quality and set aspect ratio dynamically

--- config/initializers/hydra_derivatives_config.rb
+++ config/initializers/hydra_derivatives_config.rb
@@ -1,4 +1,3 @@
 require 'hydra/derivatives'
 
 Hydra::Derivatives::Processors::Video::Processor.config.video_bitrate = '1500k'
-Hydra::Derivatives::Processors::Video::Processor.config.size_attributes = '1280x720'
